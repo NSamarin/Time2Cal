42ffa0d752f4daac0146607ea936fe8fa7b8d6de
Nikita Samarin <N.Samarin.S@gmail.com>
2015-09-20 16:27:49 +0100

    Changed app.js

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var users = require('./routes/users');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+//app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 3002);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -135,7 +137,13 @@ app.use(function (err, req, res, next) {
     });
 });
 
-app.listen(app.get('port'));
+//app.listen(app.get('port'));
+
+http.createServer(app).listen(app.get('port') ,app.get('ip'), function () {
+    console.log("Express server listening at %s:%d ", app.get('ip'),app.get('port'));
+    server();
+});
+
 console.log('Express server listening on port ' + app.get('port'));
 
 module.exports = app;
